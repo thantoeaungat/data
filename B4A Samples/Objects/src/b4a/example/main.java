b26f224944e81983f8cc6fab7fe02de580ec0aad
thantoeaungat <43115040+thantoeaungat@users.noreply.github.com>
2020-04-11 13:23:51 +0630

    Initial commit

---
+++ B4A Samples/Objects/src/b4a/example/main.java
@@ -0,0 +1,423 @@
+package b4a.example;
+
+
+import anywheresoftware.b4a.B4AMenuItem;
+import android.app.Activity;
+import android.os.Bundle;
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.BALayout;
+import anywheresoftware.b4a.B4AActivity;
+import anywheresoftware.b4a.ObjectWrapper;
+import anywheresoftware.b4a.objects.ActivityWrapper;
+import java.lang.reflect.InvocationTargetException;
+import anywheresoftware.b4a.B4AUncaughtException;
+import anywheresoftware.b4a.debug.*;
+import java.lang.ref.WeakReference;
+
+public class main extends Activity implements B4AActivity{
+	public static main mostCurrent;
+	static boolean afterFirstLayout;
+	static boolean isFirst = true;
+    private static boolean processGlobalsRun = false;
+	BALayout layout;
+	public static BA processBA;
+	BA activityBA;
+    ActivityWrapper _activity;
+    java.util.ArrayList<B4AMenuItem> menuItems;
+	public static final boolean fullScreen = false;
+	public static final boolean includeTitle = true;
+    public static WeakReference<Activity> previousOne;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        mostCurrent = this;
+		if (processBA == null) {
+			processBA = new BA(this.getApplicationContext(), null, null, "b4a.example", "b4a.example.main");
+			processBA.loadHtSubs(this.getClass());
+	        float deviceScale = getApplicationContext().getResources().getDisplayMetrics().density;
+	        BALayout.setDeviceScale(deviceScale);
+            
+		}
+		else if (previousOne != null) {
+			Activity p = previousOne.get();
+			if (p != null && p != this) {
+                BA.LogInfo("Killing previous instance (main).");
+				p.finish();
+			}
+		}
+        processBA.setActivityPaused(true);
+        processBA.runHook("oncreate", this, null);
+		if (!includeTitle) {
+        	this.getWindow().requestFeature(android.view.Window.FEATURE_NO_TITLE);
+        }
+        if (fullScreen) {
+        	getWindow().setFlags(android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN,   
+        			android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+		
+        processBA.sharedProcessBA.activityBA = null;
+		layout = new BALayout(this);
+		setContentView(layout);
+		afterFirstLayout = false;
+        WaitForLayout wl = new WaitForLayout();
+        if (anywheresoftware.b4a.objects.ServiceHelper.StarterHelper.startFromActivity(this, processBA, wl, false))
+		    BA.handler.postDelayed(wl, 5);
+
+	}
+	static class WaitForLayout implements Runnable {
+		public void run() {
+			if (afterFirstLayout)
+				return;
+			if (mostCurrent == null)
+				return;
+            
+			if (mostCurrent.layout.getWidth() == 0) {
+				BA.handler.postDelayed(this, 5);
+				return;
+			}
+			mostCurrent.layout.getLayoutParams().height = mostCurrent.layout.getHeight();
+			mostCurrent.layout.getLayoutParams().width = mostCurrent.layout.getWidth();
+			afterFirstLayout = true;
+			mostCurrent.afterFirstLayout();
+		}
+	}
+	private void afterFirstLayout() {
+        if (this != mostCurrent)
+			return;
+		activityBA = new BA(this, layout, processBA, "b4a.example", "b4a.example.main");
+        
+        processBA.sharedProcessBA.activityBA = new java.lang.ref.WeakReference<BA>(activityBA);
+        anywheresoftware.b4a.objects.ViewWrapper.lastId = 0;
+        _activity = new ActivityWrapper(activityBA, "activity");
+        anywheresoftware.b4a.Msgbox.isDismissing = false;
+        if (BA.isShellModeRuntimeCheck(processBA)) {
+			if (isFirst)
+				processBA.raiseEvent2(null, true, "SHELL", false);
+			processBA.raiseEvent2(null, true, "CREATE", true, "b4a.example.main", processBA, activityBA, _activity, anywheresoftware.b4a.keywords.Common.Density, mostCurrent);
+			_activity.reinitializeForShell(activityBA, "activity");
+		}
+        initializeProcessGlobals();		
+        initializeGlobals();
+        
+        BA.LogInfo("** Activity (main) Create, isFirst = " + isFirst + " **");
+        processBA.raiseEvent2(null, true, "activity_create", false, isFirst);
+		isFirst = false;
+		if (this != mostCurrent)
+			return;
+        processBA.setActivityPaused(false);
+        BA.LogInfo("** Activity (main) Resume **");
+        processBA.raiseEvent(null, "activity_resume");
+        if (android.os.Build.VERSION.SDK_INT >= 11) {
+			try {
+				android.app.Activity.class.getMethod("invalidateOptionsMenu").invoke(this,(Object[]) null);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	public void addMenuItem(B4AMenuItem item) {
+		if (menuItems == null)
+			menuItems = new java.util.ArrayList<B4AMenuItem>();
+		menuItems.add(item);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+		super.onCreateOptionsMenu(menu);
+        try {
+            if (processBA.subExists("activity_actionbarhomeclick")) {
+                Class.forName("android.app.ActionBar").getMethod("setHomeButtonEnabled", boolean.class).invoke(
+                    getClass().getMethod("getActionBar").invoke(this), true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (processBA.runHook("oncreateoptionsmenu", this, new Object[] {menu}))
+            return true;
+		if (menuItems == null)
+			return false;
+		for (B4AMenuItem bmi : menuItems) {
+			android.view.MenuItem mi = menu.add(bmi.title);
+			if (bmi.drawable != null)
+				mi.setIcon(bmi.drawable);
+            if (android.os.Build.VERSION.SDK_INT >= 11) {
+				try {
+                    if (bmi.addToBar) {
+				        android.view.MenuItem.class.getMethod("setShowAsAction", int.class).invoke(mi, 1);
+                    }
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			mi.setOnMenuItemClickListener(new B4AMenuItemsClickListener(bmi.eventName.toLowerCase(BA.cul)));
+		}
+        
+		return true;
+	}   
+ @Override
+ public boolean onOptionsItemSelected(android.view.MenuItem item) {
+    if (item.getItemId() == 16908332) {
+        processBA.raiseEvent(null, "activity_actionbarhomeclick");
+        return true;
+    }
+    else
+        return super.onOptionsItemSelected(item); 
+}
+@Override
+ public boolean onPrepareOptionsMenu(android.view.Menu menu) {
+    super.onPrepareOptionsMenu(menu);
+    processBA.runHook("onprepareoptionsmenu", this, new Object[] {menu});
+    return true;
+    
+ }
+ protected void onStart() {
+    super.onStart();
+    processBA.runHook("onstart", this, null);
+}
+ protected void onStop() {
+    super.onStop();
+    processBA.runHook("onstop", this, null);
+}
+    public void onWindowFocusChanged(boolean hasFocus) {
+       super.onWindowFocusChanged(hasFocus);
+       if (processBA.subExists("activity_windowfocuschanged"))
+           processBA.raiseEvent2(null, true, "activity_windowfocuschanged", false, hasFocus);
+    }
+	private class B4AMenuItemsClickListener implements android.view.MenuItem.OnMenuItemClickListener {
+		private final String eventName;
+		public B4AMenuItemsClickListener(String eventName) {
+			this.eventName = eventName;
+		}
+		public boolean onMenuItemClick(android.view.MenuItem item) {
+			processBA.raiseEventFromUI(item.getTitle(), eventName + "_click");
+			return true;
+		}
+	}
+    public static Class<?> getObject() {
+		return main.class;
+	}
+    private Boolean onKeySubExist = null;
+    private Boolean onKeyUpSubExist = null;
+	@Override
+	public boolean onKeyDown(int keyCode, android.view.KeyEvent event) {
+        if (processBA.runHook("onkeydown", this, new Object[] {keyCode, event}))
+            return true;
+		if (onKeySubExist == null)
+			onKeySubExist = processBA.subExists("activity_keypress");
+		if (onKeySubExist) {
+			if (keyCode == anywheresoftware.b4a.keywords.constants.KeyCodes.KEYCODE_BACK &&
+					android.os.Build.VERSION.SDK_INT >= 18) {
+				HandleKeyDelayed hk = new HandleKeyDelayed();
+				hk.kc = keyCode;
+				BA.handler.post(hk);
+				return true;
+			}
+			else {
+				boolean res = new HandleKeyDelayed().runDirectly(keyCode);
+				if (res)
+					return true;
+			}
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+	private class HandleKeyDelayed implements Runnable {
+		int kc;
+		public void run() {
+			runDirectly(kc);
+		}
+		public boolean runDirectly(int keyCode) {
+			Boolean res =  (Boolean)processBA.raiseEvent2(_activity, false, "activity_keypress", false, keyCode);
+			if (res == null || res == true) {
+                return true;
+            }
+            else if (keyCode == anywheresoftware.b4a.keywords.constants.KeyCodes.KEYCODE_BACK) {
+				finish();
+				return true;
+			}
+            return false;
+		}
+		
+	}
+    @Override
+	public boolean onKeyUp(int keyCode, android.view.KeyEvent event) {
+        if (processBA.runHook("onkeyup", this, new Object[] {keyCode, event}))
+            return true;
+		if (onKeyUpSubExist == null)
+			onKeyUpSubExist = processBA.subExists("activity_keyup");
+		if (onKeyUpSubExist) {
+			Boolean res =  (Boolean)processBA.raiseEvent2(_activity, false, "activity_keyup", false, keyCode);
+			if (res == null || res == true)
+				return true;
+		}
+		return super.onKeyUp(keyCode, event);
+	}
+	@Override
+	public void onNewIntent(android.content.Intent intent) {
+        super.onNewIntent(intent);
+		this.setIntent(intent);
+        processBA.runHook("onnewintent", this, new Object[] {intent});
+	}
+    @Override 
+	public void onPause() {
+		super.onPause();
+        if (_activity == null)
+            return;
+        if (this != mostCurrent)
+			return;
+		anywheresoftware.b4a.Msgbox.dismiss(true);
+        BA.LogInfo("** Activity (main) Pause, UserClosed = " + activityBA.activity.isFinishing() + " **");
+        if (mostCurrent != null)
+            processBA.raiseEvent2(_activity, true, "activity_pause", false, activityBA.activity.isFinishing());		
+        processBA.setActivityPaused(true);
+        mostCurrent = null;
+        if (!activityBA.activity.isFinishing())
+			previousOne = new WeakReference<Activity>(this);
+        anywheresoftware.b4a.Msgbox.isDismissing = false;
+        processBA.runHook("onpause", this, null);
+	}
+
+	@Override
+	public void onDestroy() {
+        super.onDestroy();
+		previousOne = null;
+        processBA.runHook("ondestroy", this, null);
+	}
+    @Override 
+	public void onResume() {
+		super.onResume();
+        mostCurrent = this;
+        anywheresoftware.b4a.Msgbox.isDismissing = false;
+        if (activityBA != null) { //will be null during activity create (which waits for AfterLayout).
+        	ResumeMessage rm = new ResumeMessage(mostCurrent);
+        	BA.handler.post(rm);
+        }
+        processBA.runHook("onresume", this, null);
+	}
+    private static class ResumeMessage implements Runnable {
+    	private final WeakReference<Activity> activity;
+    	public ResumeMessage(Activity activity) {
+    		this.activity = new WeakReference<Activity>(activity);
+    	}
+		public void run() {
+            main mc = mostCurrent;
+			if (mc == null || mc != activity.get())
+				return;
+			processBA.setActivityPaused(false);
+            BA.LogInfo("** Activity (main) Resume **");
+            if (mc != mostCurrent)
+                return;
+		    processBA.raiseEvent(mc._activity, "activity_resume", (Object[])null);
+		}
+    }
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode,
+	      android.content.Intent data) {
+		processBA.onActivityResult(requestCode, resultCode, data);
+        processBA.runHook("onactivityresult", this, new Object[] {requestCode, resultCode});
+	}
+	private static void initializeGlobals() {
+		processBA.raiseEvent2(null, true, "globals", false, (Object[])null);
+	}
+    public void onRequestPermissionsResult(int requestCode,
+        String permissions[], int[] grantResults) {
+        for (int i = 0;i < permissions.length;i++) {
+            Object[] o = new Object[] {permissions[i], grantResults[i] == 0};
+            processBA.raiseEventFromDifferentThread(null,null, 0, "activity_permissionresult", true, o);
+        }
+            
+    }
+
+public anywheresoftware.b4a.keywords.Common __c = null;
+public static anywheresoftware.b4a.objects.SimpleExoPlayerWrapper _player1 = null;
+public anywheresoftware.b4a.objects.SimpleExoPlayerViewWrapper _simpleexoplayerview1 = null;
+public b4a.example.starter _starter = null;
+
+public static boolean isAnyActivityVisible() {
+    boolean vis = false;
+vis = vis | (main.mostCurrent != null);
+return vis;}
+public static String  _activity_create(boolean _firsttime) throws Exception{
+anywheresoftware.b4a.objects.collections.List _sources = null;
+ //BA.debugLineNum = 23;BA.debugLine="Sub Activity_Create(FirstTime As Boolean)";
+ //BA.debugLineNum = 24;BA.debugLine="If FirstTime Then";
+if (_firsttime) { 
+ //BA.debugLineNum = 25;BA.debugLine="player1.Initialize(\"player\")";
+_player1.Initialize(processBA,"player");
+ //BA.debugLineNum = 26;BA.debugLine="Dim sources As List";
+_sources = new anywheresoftware.b4a.objects.collections.List();
+ //BA.debugLineNum = 27;BA.debugLine="sources.Initialize";
+_sources.Initialize();
+ //BA.debugLineNum = 28;BA.debugLine="sources.Add(player1.CreateUriSource(\"https://r5-";
+_sources.Add(_player1.CreateUriSource("https://r5---sn-30a7rn76.googlevideo.com/videoplayback?expire=1586609068&ei=TGeRXrbiKIffgAfov45w&ip=156.202.245.33&id=o-AJ052r3INdrD_gt_jeR8TiUK6ILBEvFSGcVLofpJH6Px&itag=22&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&ratebypass=yes&dur=335.458&lmt=1537652280179019&fvip=5&c=WEB&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cratebypass%2Cdur%2Clmt&sig=AJpPlLswRAIgSnp3fachscHE9Vxo3nKE-V7DXKLTlK1Q-7im21p4B8ICIEGy3vpsdP2MbJ8hNKxA8pdr55OAtO-u_0OMklERc4Iw&contentlength=23296973&video_id=OfOMEP_7AJM&title=The+History+of+Thailand+Explained+in+5+minutes&rm=sn-uxaxjvhxbt2u-amgl7s,sn-hgnls7z&req_id=d413f6f15574a3ee&redirect_counter=2&cms_redirect=yes&ipbypass=yes&mh=Po&mip=103.25.243.172&mm=29&mn=sn-30a7rn76&ms=rdu&mt=1586587411&mv=m&mvi=4&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=ALrAebAwRgIhAJcILO5Q4891C04oyWyjBftOxcm5UVfANlwCC0kJKYYuAiEA0oCtinrAcaxuyNYcUPDFfAMdH6SuBHLKxEx_3J4XqIk%3D"));
+ //BA.debugLineNum = 29;BA.debugLine="player1.Prepare(player1.CreateListSource(sources";
+_player1.Prepare(_player1.CreateListSource(_sources));
+ };
+ //BA.debugLineNum = 32;BA.debugLine="Activity.LoadLayout(\"1\")";
+mostCurrent._activity.LoadLayout("1",mostCurrent.activityBA);
+ //BA.debugLineNum = 33;BA.debugLine="SimpleExoPlayerView1.Player = player1";
+mostCurrent._simpleexoplayerview1.setPlayer(_player1);
+ //BA.debugLineNum = 34;BA.debugLine="player1.Play";
+_player1.Play();
+ //BA.debugLineNum = 35;BA.debugLine="End Sub";
+return "";
+}
+public static String  _activity_pause(boolean _userclosed) throws Exception{
+ //BA.debugLineNum = 53;BA.debugLine="Sub Activity_Pause (UserClosed As Boolean)";
+ //BA.debugLineNum = 55;BA.debugLine="End Sub";
+return "";
+}
+public static String  _activity_resume() throws Exception{
+ //BA.debugLineNum = 49;BA.debugLine="Sub Activity_Resume";
+ //BA.debugLineNum = 51;BA.debugLine="End Sub";
+return "";
+}
+public static String  _globals() throws Exception{
+ //BA.debugLineNum = 19;BA.debugLine="Sub Globals";
+ //BA.debugLineNum = 20;BA.debugLine="Private SimpleExoPlayerView1 As SimpleExoPlayerVi";
+mostCurrent._simpleexoplayerview1 = new anywheresoftware.b4a.objects.SimpleExoPlayerViewWrapper();
+ //BA.debugLineNum = 21;BA.debugLine="End Sub";
+return "";
+}
+public static String  _player_complete() throws Exception{
+ //BA.debugLineNum = 45;BA.debugLine="Sub Player_Complete";
+ //BA.debugLineNum = 46;BA.debugLine="Log(\"complete\")";
+anywheresoftware.b4a.keywords.Common.LogImpl("8851969","complete",0);
+ //BA.debugLineNum = 47;BA.debugLine="End Sub";
+return "";
+}
+public static String  _player_error(String _message) throws Exception{
+ //BA.debugLineNum = 41;BA.debugLine="Sub Player_Error (Message As String)";
+ //BA.debugLineNum = 42;BA.debugLine="Log(\"Error: \" & Message)";
+anywheresoftware.b4a.keywords.Common.LogImpl("8786433","Error: "+_message,0);
+ //BA.debugLineNum = 43;BA.debugLine="End Sub";
+return "";
+}
+public static String  _player_ready() throws Exception{
+ //BA.debugLineNum = 37;BA.debugLine="Sub Player_Ready";
+ //BA.debugLineNum = 38;BA.debugLine="Log(\"Ready\")";
+anywheresoftware.b4a.keywords.Common.LogImpl("8720897","Ready",0);
+ //BA.debugLineNum = 39;BA.debugLine="End Sub";
+return "";
+}
+
+public static void initializeProcessGlobals() {
+    
+    if (main.processGlobalsRun == false) {
+	    main.processGlobalsRun = true;
+		try {
+		        main._process_globals();
+starter._process_globals();
+		
+        } catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+    }
+}public static String  _process_globals() throws Exception{
+ //BA.debugLineNum = 15;BA.debugLine="Sub Process_Globals";
+ //BA.debugLineNum = 16;BA.debugLine="Private player1 As SimpleExoPlayer";
+_player1 = new anywheresoftware.b4a.objects.SimpleExoPlayerWrapper();
+ //BA.debugLineNum = 17;BA.debugLine="End Sub";
+return "";
+}
+}
